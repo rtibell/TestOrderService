9e97aab421994fdd41c9b24ea27d77ac26b83641
rasmus.tibell <rasmus.tibell@gmail.com>
2024-04-17 23:11:11 +0200

    mod receipt request.

--- src/main/java/se/consid/order/controller/OrderController.java
+++ src/main/java/se/consid/order/controller/OrderController.java
@@ -1,5 +1,6 @@
 package se.consid.order.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 public class OrderController {
     @Autowired
